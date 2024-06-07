f4eb10a62049c3688e27b4a96b3f2b2acf910f08
Jespercal <chap600@hotmail.com>
2024-06-07 19:55:47 +0200

    Version 2

--- js/simple_graph.js
+++ js/simple_graph.js
@@ -3,6 +3,7 @@ function SVG(t) {
 }
 var drawGraph = function() {
 		var circles = [],lastAt = -1;
+		var circle_ys = {};
         var t = dayjs.unix(latestAt),
             e = '',
             e2 = '',
@@ -12,35 +13,37 @@ var drawGraph = function() {
             let a = 360 - 3 * t;
             r += 20,
 			e += ('' == e ? '' : ' ') + r + ',' + a,
-			circles.push([r, a, t])
+			circles.push([r, a, t,'red'])
+			circle_ys[r] = [t,0];
         });
 		values2.forEach(t => {
             let a = 360 - 3 * t;
             r2 += 20,
 			e2 += ('' == e2 ? '' : ' ') + r2 + ',' + a,
-			circles.push([r2, a, t])
+			circles.push([r2, a, t,'blue'])
+			circle_ys[r2][1] = t;
         });
         for (let a = 1; a < 20; a++){
 			$(SVG('line')).attr('x1', 60 + 20 * a).attr('x2', 60 + 20 * a).attr('y1', 40).attr('y2', 360).attr('stroke-width', '0.2').attr('stroke', 'rgba(0,0,0,0.5)').appendTo(l);
 		}
         console.log(e);
-        var l = $('.graph svg');
+        var l = $('.graph svg > g');
         $(SVG('polyline')).attr('points', e).attr('style', 'stroke:red;stroke-width:2;fill:transparent;').appendTo(l)
         $(SVG('polyline')).attr('points', e2).attr('style', 'stroke:blue;stroke-width:2;fill:transparent;').appendTo(l)
 		$(SVG('polyline')).attr('points', '40,360 ' + e + ' ' + r + ',360').attr('style', 'fill:rgba(255,0,0,0.1);').appendTo(l)
 		$(SVG('polyline')).attr('points', '40,360 ' + e2 + ' ' + r2 + ',360').attr('style', 'fill:rgba(0,0,255,0.1);').appendTo(l)
 		circles.forEach(t => {
-            $(SVG('circle')).attr('cx', t[0]).attr('cy', t[1]).attr('r', 3).attr('stroke', 'transparent').attr('stroke-width', '20').click(function() {
-                if (lastAt != t[0]) {
+            $(SVG('circle')).attr('cx', t[0]).attr('cy', t[1]).attr('r', 3).attr('fill', t[3]).attr('stroke', 'black').attr('stroke-width', '1').click(function() {
+                if (lastAt != t[0]) {	
                     lastAt = t[0];
                     let e = $(this).position();
                     $('#hoverer').css({
                         top: e.top - 48,
-                        left: e.left - 22,
+                        left: e.left - 24,
                         position: 'absolute'
                     })
-					$('#hoverer span:first-child').text(t[2] + '%')
-					$('#hoverer span:last-child').text(t[2] + '°')
+					$('#hoverer span:first-child').text(circle_ys[t[0]][0] + '%')
+					$('#hoverer span:last-child').text(circle_ys[t[0]][1] + '°')
                 } else lastAt = !1, $('#hoverer').css({
                     top: -100,
                     left: -100,
@@ -51,7 +54,7 @@ var drawGraph = function() {
         for (let n = 0; n < 11; n++) {
             let s = 40 + 32 * n;
             $(SVG('line')).attr('x1', 35).attr('x2', 45).attr('y1', s).attr('y2', s).attr('stroke-width', 1).attr('stroke', 'black').appendTo(l)
-			$(SVG('text')).attr('x', 5).attr('y', s).text(100 - 10 * n + '%%').attr('style', 'alignment-baseline: central;font-size:12;').appendTo(l)
+			$(SVG('text')).attr('x', 5).attr('y', s).text(100 - 10 * n + '%').attr('style', 'alignment-baseline: central;font-size:12;').appendTo(l)
         }
         for (let o = 0; o < Math.ceil(values.length / 2); o++) {
             let i = t.clone();
